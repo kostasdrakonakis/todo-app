a73c0c336160694ba88d0316cae1aa34be9b597f
kostasdrakonakis <kostasdrakonakis13@gmail.com>
2022-03-20 14:58:14 +0100

    test commit

---
+++ annotations/src/main/java/com/github/kostasdrakonakis/annotation/IntentType.java
@@ -0,0 +1,28 @@
+package com.github.kostasdrakonakis.annotation;
+
+/**
+ * Declare the type for {@literal @}{@link IntentExtra}
+ */
+public enum IntentType {
+    INT,
+    LONG,
+    FLOAT,
+    SHORT,
+    STRING,
+    BOOLEAN,
+    BYTE,
+    CHAR,
+    DOUBLE,
+    BOOLEAN_ARRAY,
+    BYTE_ARRAY,
+    CHAR_ARRAY,
+    CHAR_SEQUENCE,
+    CHAR_SEQUENCE_ARRAY,
+    LONG_ARRAY,
+    INT_ARRAY,
+    SHORT_ARRAY,
+    STRING_ARRAY,
+    SERIALIZABLE,
+    BUNDLE,
+    PARCELABLE
+}
